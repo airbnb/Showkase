ff903c0d46dc01bd7d5d491c5296024e3a3d2bf4
vinaygaba <vinaygaba@gmail.com>
2020-04-21 18:25:01 -0700

    First commit

---
+++ app/src/main/java/com/vinaygaba/showcase/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.vinaygaba.showcase
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        Greeting("Android")
+    }
+}
