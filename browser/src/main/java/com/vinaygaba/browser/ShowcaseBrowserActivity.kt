e05866f758413694ed1eb34d1637ed0c2f3440f1
vinaygaba <vinaygaba@gmail.com>
2020-05-14 23:48:36 -0700

    NitS

--- browser/src/main/java/com/vinaygaba/browser/ShowcaseBrowserActivity.kt
+++ browser/src/main/java/com/vinaygaba/browser/ShowcaseBrowserActivity.kt
@@ -1,7 +1,6 @@
 package com.vinaygaba.browser
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.remember
