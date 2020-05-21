9eaa9de468044fdda4f6b11d5817f44041ba94b3
vinaygaba <vinaygaba@gmail.com>
2020-05-21 09:01:43 -0700

    Publish working, not combiling though

--- app/src/main/java/com/vinaygaba/showcase/MainActivity.kt
+++ app/src/main/java/com/vinaygaba/showcase/MainActivity.kt
@@ -4,14 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.setContent
-import com.vinaygaba.browser.ui.ShowcaseBrowserActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val intent = Intent(this, ShowcaseBrowserActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this, ShowcaseBrowserActivity::class.java)
+//            startActivity(intent)
         }
     }
 }
