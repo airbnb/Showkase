d0fffc06e17510f32fc666615ca4bb64a661982f
Vinay Gaba <vinaygaba@gmail.com>
2020-05-22 23:03:53 -0700

    Made group/name a required field (#22)

--- showcase-annotation/src/main/java/com/vinaygaba/showcase/annotation/models/Showcase.kt
+++ showcase-annotation/src/main/java/com/vinaygaba/showcase/annotation/models/Showcase.kt
@@ -4,8 +4,8 @@ package com.vinaygaba.showcase.annotation.models
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.TYPE)
 annotation class Showcase(
-    val name: String = "",
-    val group: String = "",
+    val name: String,
+    val group: String,
     val widthDp: Int = -1,
     val heightDp: Int = -1
 )
