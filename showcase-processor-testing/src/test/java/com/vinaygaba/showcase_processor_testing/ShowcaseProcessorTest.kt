fbf0000b6d0f7f1a35d4dda4c8655a89954162d0
vinaygaba <vinaygaba@gmail.com>
2020-06-06 22:58:07 -0700

    Added more unit tests

--- showcase-processor-testing/src/test/java/com/vinaygaba/showcase_processor_testing/ShowcaseProcessorTest.kt
+++ showcase-processor-testing/src/test/java/com/vinaygaba/showcase_processor_testing/ShowcaseProcessorTest.kt
@@ -1,6 +1,6 @@
 package com.vinaygaba.showcase_processor_testing
 
-import com.google.common.truth.Truth.assertThat
+import org.assertj.core.api.Assertions.assertThat
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
 import com.vinaygaba.showcase.processor.ShowcaseProcessor
@@ -180,5 +180,97 @@ class ShowcaseProcessorTest {
         val error = "Make sure that the @Composable functions that you annotate with the " +
                 "@Showcase annotation do not take in any parameters"
         assertThat(result.messages.contains(error))
+    }
+
+    @Test
+    fun `top level composable function with showcase annotation generates correct file`() {
+        val kotlinSource = SourceFile.kotlin("GeneratedTestComposables.kt", """
+        package com.vinaygaba.showcase_processor_testing
+        
+        import com.vinaygaba.showcase.annotation.models.Showcase
+        import androidx.compose.Composable
+        
+        @Showcase("group", "name")
+        @Composable
+        fun TestComposable() {
+            
+        }
+    """)
+        val result = KotlinCompilation().apply {
+            sources = listOf(kotlinSource)
+            annotationProcessors = listOf(ShowcaseProcessor())
+            inheritClassPath = true
+            messageOutputStream = System.out // see diagnostics in real time
+        }.compile()
+
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+        result.sourcesGeneratedByAnnotationProcessor.forEach { 
+            assertThat(it).hasContent("""
+                // This is an auto-generated file. Please do not edit/modify this file.
+                package com.vinaygaba.showcase
+                
+                import androidx.compose.Composable
+                import com.vinaygaba.showcase.models.ShowcaseCodegenMetadata
+                import com.vinaygaba.showcase_processor_testing.TestComposable
+                import kotlin.collections.List
+                
+                class ShowcaseCodegenComponents {
+                  val componentList: List<ShowcaseCodegenMetadata> = listOf<ShowcaseCodegenMetadata>(
+                    ShowcaseCodegenMetadata("name", "group", -1, -1, 
+                    @Composable { TestComposable() }))
+                  }
+            """.trimIndent()
+            )
+        }
+    }
+
+    @Test
+    fun `multiple composable functions with showcase annotations generates correct file`() {
+        val kotlinSource = SourceFile.kotlin("GeneratedTestComposables.kt", """
+        package com.vinaygaba.showcase_processor_testing
+        
+        import com.vinaygaba.showcase.annotation.models.Showcase
+        import androidx.compose.Composable
+        
+        @Showcase("group1", "name1")
+        @Composable
+        fun TestComposable1() {
+            
+        }
+        
+        @Showcase("group1", "name2")
+        @Composable
+        fun TestComposable2() {
+            
+        }
+    """)
+        val result = KotlinCompilation().apply {
+            sources = listOf(kotlinSource)
+            annotationProcessors = listOf(ShowcaseProcessor())
+            inheritClassPath = true
+            messageOutputStream = System.out // see diagnostics in real time
+        }.compile()
+
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+        result.sourcesGeneratedByAnnotationProcessor.forEach {
+            assertThat(it).hasContent("""
+                // This is an auto-generated file. Please do not edit/modify this file.
+                package com.vinaygaba.showcase
+                
+                import androidx.compose.Composable
+                import com.vinaygaba.showcase.models.ShowcaseCodegenMetadata
+                import com.vinaygaba.showcase_processor_testing.TestComposable1
+                import com.vinaygaba.showcase_processor_testing.TestComposable2
+                import kotlin.collections.List
+                
+                class ShowcaseCodegenComponents {
+                  val componentList: List<ShowcaseCodegenMetadata> = listOf<ShowcaseCodegenMetadata>(
+                    ShowcaseCodegenMetadata("name1", "group1", -1, -1, 
+                    @Composable { TestComposable1() }),ShowcaseCodegenMetadata("name2", "group1", -1, -1, 
+                    @Composable { TestComposable2() }))
+                  }
+            """.trimIndent()
+            )
+        }
     }
 }
