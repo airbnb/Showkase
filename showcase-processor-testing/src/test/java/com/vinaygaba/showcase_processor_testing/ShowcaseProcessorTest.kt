f522103c27bb55aa1e31ff3f062dc837069c3ffd
Vinay Gaba <vinaygaba@gmail.com>
2020-06-06 20:12:05 -0700

    Setup for testing the annotation processor (#37)
    * Setup for annotation testing
    
    * Intentionally fail test
    
    * Fix tests again
    
    * Fix duplicate META-INF file issue
    
    * Fix META-INF issue

---
+++ showcase-processor-testing/src/test/java/com/vinaygaba/showcase_processor_testing/ShowcaseProcessorTest.kt
@@ -0,0 +1,33 @@
+package com.vinaygaba.showcase_processor_testing
+
+import com.google.common.truth.Truth.assertThat
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import com.vinaygaba.showcase.processor.ShowcaseProcessor
+import org.junit.Test
+
+class ShowcaseProcessorTest {
+    @Test
+    fun `basic processor test`() {
+        val kotlinSource = SourceFile.kotlin("Composables.kt", """
+        import com.vinaygaba.showcase.annotation.models.Showcase
+        import androidx.compose.Composable
+        
+        class Composables {
+            @Showcase("group", "name")
+            @Composable
+            fun TestComposable() {
+                
+            }
+        }
+    """)
+        val result = KotlinCompilation().apply {
+            sources = listOf(kotlinSource)
+            annotationProcessors = listOf(ShowcaseProcessor())
+            inheritClassPath = true
+            messageOutputStream = System.out // see diagnostics in real time
+        }.compile()
+
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+    }
+}
