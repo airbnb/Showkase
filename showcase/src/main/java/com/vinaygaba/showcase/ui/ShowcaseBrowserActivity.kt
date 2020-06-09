1380ff21c962337541a99d0be5b99509f03667b2
vinaygaba <vinaygaba@gmail.com>
2020-06-08 22:19:06 -0700

    Implemented proper way to navigating back

--- showcase/src/main/java/com/vinaygaba/showcase/ui/ShowcaseBrowserActivity.kt
+++ showcase/src/main/java/com/vinaygaba/showcase/ui/ShowcaseBrowserActivity.kt
@@ -3,9 +3,7 @@ package com.vinaygaba.showcase.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.setContent
-import com.vinaygaba.showcase.models.ShowcaseBrowserScreenMetadata
 import com.vinaygaba.showcase.models.ShowcaseCodegenMetadata
-import com.vinaygaba.showcase.models.ShowcaseCurrentScreen
 
 class ShowcaseBrowserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,30 +39,30 @@ class ShowcaseBrowserActivity : AppCompatActivity() {
         }
     }
 
-    override fun onBackPressed() {
-        val currentScreen = ShowcaseBrowserScreenMetadata.currentScreen
-        val isSearchActive = ShowcaseBrowserScreenMetadata.isSearchActive
-        when {
-            isSearchActive -> {
-                ShowcaseBrowserScreenMetadata.isSearchActive = false
-                ShowcaseBrowserScreenMetadata.searchQuery = null
-            }
-            currentScreen == ShowcaseCurrentScreen.GROUPS -> {
-                finish()
-            }
-            currentScreen == ShowcaseCurrentScreen.GROUP_COMPONENTS -> {
-                ShowcaseBrowserScreenMetadata.currentScreen =
-                    ShowcaseCurrentScreen.GROUPS
-                ShowcaseBrowserScreenMetadata.currentGroup = null
-                ShowcaseBrowserScreenMetadata.currentComponent = null
-            }
-            currentScreen == ShowcaseCurrentScreen.COMPONENT_DETAIL -> {
-                ShowcaseBrowserScreenMetadata.currentScreen =
-                    ShowcaseCurrentScreen.GROUP_COMPONENTS
-                ShowcaseBrowserScreenMetadata.currentComponent = null
-            }
-        }
-    }
+//    override fun onBackPressed() {
+//        val currentScreen = ShowcaseBrowserScreenMetadata.currentScreen
+//        val isSearchActive = ShowcaseBrowserScreenMetadata.isSearchActive
+//        when {
+//            isSearchActive -> {
+//                ShowcaseBrowserScreenMetadata.isSearchActive = false
+//                ShowcaseBrowserScreenMetadata.searchQuery = null
+//            }
+//            currentScreen == ShowcaseCurrentScreen.GROUPS -> {
+//                finish()
+//            }
+//            currentScreen == ShowcaseCurrentScreen.GROUP_COMPONENTS -> {
+//                ShowcaseBrowserScreenMetadata.currentScreen =
+//                    ShowcaseCurrentScreen.GROUPS
+//                ShowcaseBrowserScreenMetadata.currentGroup = null
+//                ShowcaseBrowserScreenMetadata.currentComponent = null
+//            }
+//            currentScreen == ShowcaseCurrentScreen.COMPONENT_DETAIL -> {
+//                ShowcaseBrowserScreenMetadata.currentScreen =
+//                    ShowcaseCurrentScreen.GROUP_COMPONENTS
+//                ShowcaseBrowserScreenMetadata.currentComponent = null
+//            }
+//        }
+//    }
 
     companion object {
         const val CODEGEN_PACKAGE_NAME = "com.vinaygaba.showcase"
