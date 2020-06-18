ffb5cc365263c3a4e989de16a23206c89415d87c
Vinay Gaba <vinaygaba@gmail.com>
2020-06-18 09:46:32 -0700

    Do some cleanup based on feedback (#50)
    * Cleanup
    
    * Suppress detekt issue

--- showcase-processor/src/main/java/com/vinaygaba/showcase/processor/logging/ShowcaseValidator.kt
+++ showcase-processor/src/main/java/com/vinaygaba/showcase/processor/logging/ShowcaseValidator.kt
@@ -1,6 +1,7 @@
 package com.vinaygaba.showcase.processor.logging
 
 import com.vinaygaba.showcase.annotation.models.Showcase
+import com.vinaygaba.showcase.processor.exceptions.ShowcaseProcessorException
 import javax.lang.model.element.Element
 import javax.lang.model.element.ElementKind
 import javax.lang.model.element.Modifier
@@ -9,38 +10,30 @@ import javax.lang.model.type.TypeMirror
 import javax.lang.model.util.Types
 
 class ShowcaseValidator {
+    @Suppress("ThrowsCount")
     internal fun validateElement(
         element: Element,
-        logger: ShowcaseExceptionLogger,
         composableTypeMirror: TypeMirror?,
         typeUtils: Types?
-    ): Boolean {
+    ) {
         
-        return when {
+        when {
             element.kind != ElementKind.METHOD -> {
-                logger.logMessage(
-                    "Only composable methods can be annotated " +
-                            "with ${Showcase::class.java.simpleName}"
-                )
-                false
+                throw ShowcaseProcessorException("Only composable methods can be annotated " +
+                        "with ${Showcase::class.java.simpleName}")
             }
             element.annotationMirrors.find {
                 typeUtils?.isSameType(it.annotationType, composableTypeMirror!!) ?: false
             } == null -> {
-                logger.logMessage(
-                    "Only composable methods can be annotated " +
-                            "with ${Showcase::class.java.simpleName}"
-                )
-                false
+                throw ShowcaseProcessorException("Only composable methods can be annotated " +
+                        "with ${Showcase::class.java.simpleName}")
             }
             element.modifiers.contains(Modifier.PRIVATE) -> {
-                logger.logMessage(
-                    "The methods annotated with ${Showcase::class.java.simpleName} can't be private " +
-                            "as the library won't be able to access them otherwise."
-                )
-                false
+                throw ShowcaseProcessorException("The methods annotated with " +
+                        "${Showcase::class.java.simpleName} can't be private " +
+                        "as the library won't be able to access them otherwise.")
             }
-            else -> true
+            else -> { }
         }
     }
 }
