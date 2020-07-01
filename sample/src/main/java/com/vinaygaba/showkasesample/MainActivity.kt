2473114f47c85bb2400eb4e9586666cc24f904df
Vinay Gaba <vinaygaba@gmail.com>
2020-06-30 23:41:40 -0700

    Added documentation in README and some minor improvements (#56)
    * Added installation steps to README
    
    * Added more content to README
    
    * More info

--- sample/src/main/java/com/vinaygaba/showkasesample/MainActivity.kt
+++ sample/src/main/java/com/vinaygaba/showkasesample/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.vinaygaba.showkasesample
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.setContent
 import com.vinaygaba.showkase.ui.ShowkaseBrowserActivity
-import com.vinaygaba.showkase.ui.ShowkaseBrowserActivity.Companion.SHOWKASE_ROOT_MODULE_KEY
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val intent = Intent(this, ShowkaseBrowserActivity::class.java)
-            intent.putExtra(SHOWKASE_ROOT_MODULE_KEY, RootModule::class.java.canonicalName)
-            startActivity(intent)
+            startActivity(
+                ShowkaseBrowserActivity.getIntent(this, RootModule::class.java.canonicalName!!)
+            )
         }
     }
 }
