f0deda07a8c2e839d1cc68bc3bf1f5cb94a6752d
vinaygaba <vinaygaba@gmail.com>
2020-07-08 10:48:26 -0700

    Nit

--- showkase/src/main/java/com/airbnb/showkase/exceptions/ShowkaseException.kt
+++ showkase/src/main/java/com/airbnb/showkase/exceptions/ShowkaseException.kt
@@ -2,4 +2,7 @@ package com.airbnb.showkase.exceptions
 
 import java.lang.Exception
 
+/**
+ * Used to throw an exception for Showkase specific errors. 
+ */
 internal class ShowkaseException(message: String): Exception(message)
