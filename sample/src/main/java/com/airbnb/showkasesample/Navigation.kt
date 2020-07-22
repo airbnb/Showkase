40ae1b1ef5612641ab385925bac5f9f5fa516b74
vinaygaba <vinaygaba@gmail.com>
2020-07-22 10:53:47 -0700

    Use savedInstanceState for BottomNavBar

--- sample/src/main/java/com/airbnb/showkasesample/Navigation.kt
+++ sample/src/main/java/com/airbnb/showkasesample/Navigation.kt
@@ -3,7 +3,6 @@ package com.airbnb.showkasesample
 import androidx.compose.Composable
 import androidx.compose.getValue
 import androidx.compose.setValue
-import androidx.compose.state
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
@@ -12,6 +11,7 @@ import androidx.ui.material.BottomNavigation
 import androidx.ui.material.BottomNavigationItem
 import androidx.ui.material.icons.Icons
 import androidx.ui.material.icons.filled.Favorite
+import androidx.ui.savedinstancestate.savedInstanceState
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.airbnb.showkase.annotation.models.Showkase
@@ -20,7 +20,7 @@ val listItems = listOf("Games", "Apps", "Movies", "Books")
 
 @Composable
 fun BottomNavigationAlwaysShowLabelComponent() {
-    var selectedIndex by state { 0 }
+    var selectedIndex by savedInstanceState { 0 }
 
     ShowkaseTheme {
         BottomNavigation(modifier = Modifier.padding(16.dp)) {
