bda7a226d9ac39ba25ffca404c5629a6e7e22d7c
Vinay Gaba <vinaygaba@gmail.com>
2020-07-31 16:47:37 -0700

    Add support for @Preview annotation to be used for adding composables to the Showkase Browser (#69)

--- showkase-processor/src/main/java/com/airbnb/showkase/processor/models/ShowkaseMetadata.kt
+++ showkase-processor/src/main/java/com/airbnb/showkase/processor/models/ShowkaseMetadata.kt
@@ -3,12 +3,12 @@ package com.airbnb.showkase.processor.models
 import com.airbnb.showkase.annotation.models.Showkase
 import com.airbnb.showkase.annotation.models.ShowkaseCodegenMetadata
 import com.airbnb.showkase.processor.exceptions.ShowkaseProcessorException
-import javax.lang.model.element.Element
 import javax.lang.model.element.ExecutableElement
 import javax.lang.model.element.Modifier
 import javax.lang.model.type.MirroredTypesException
 import javax.lang.model.type.TypeMirror
 import javax.lang.model.util.Elements
+import javax.lang.model.util.Types
 
 internal data class ShowkaseMetadata(
     val moduleName: String,
@@ -17,8 +17,8 @@ internal data class ShowkaseMetadata(
     val methodName: String,
     val showkaseComponentName: String,
     val showkaseComponentGroup: String,
-    val showkaseComponentWidthDp: Int,
-    val showkaseComponentHeightDp: Int
+    val showkaseComponentWidthDp: Int? = null,
+    val showkaseComponentHeightDp: Int? = null
 )
 
 internal fun ShowkaseCodegenMetadata.toModel(): ShowkaseMetadata {
@@ -36,25 +36,30 @@ internal fun ShowkaseCodegenMetadata.toModel(): ShowkaseMetadata {
         methodName = composableMethodName,
         showkaseComponentName = showkaseComposableName,
         showkaseComponentGroup = showkaseComposableGroup,
-        showkaseComponentWidthDp = showkaseComposableWidthDp,
-        showkaseComponentHeightDp = showkaseComposableHeightDp
+        showkaseComponentWidthDp = showkaseComposableWidthDp.parseAnnotationProperty(),
+        showkaseComponentHeightDp = showkaseComposableHeightDp.parseAnnotationProperty()
     )
 }
 
+private fun Int.parseAnnotationProperty() = when(this) {
+    -1 -> null
+    else -> this
+}
+
 internal fun getShowkaseMetadata(
-    element: Element,
+    element: ExecutableElement,
     elementUtil: Elements
 ): ShowkaseMetadata {
-    val executableElement = element as ExecutableElement
     val enclosingElement = element.enclosingElement
-    val isStaticMethod = executableElement.modifiers.contains(Modifier.STATIC)
-    val showkaseAnnotation = executableElement.getAnnotation(Showkase::class.java)
-    val moduleName = elementUtil.getPackageOf(executableElement).simpleName.toString()
-    val packageName = element.enclosingElement.enclosingElement.asType().toString()
-    val methodName = executableElement.simpleName.toString()
+    val isStaticMethod = element.modifiers.contains(Modifier.STATIC)
+    val showkaseAnnotation = element.getAnnotation(Showkase::class.java)
+    val packageElement = elementUtil.getPackageOf(element)
+    val moduleName = packageElement.simpleName.toString()
+    val packageName = packageElement.qualifiedName.toString()
+    val methodName = element.simpleName.toString()
 
-    val noOfParameters = executableElement.parameters.size
-    if (noOfParameters > 0) {
+    val numParameters = element.parameters.size
+    if (numParameters > 0) {
         throw ShowkaseProcessorException(
             "Make sure that the @Composable functions that you " +
                     "annotate with the @Showkase annotation do not take in any parameters"
@@ -72,7 +77,66 @@ internal fun getShowkaseMetadata(
         methodName = methodName,
         showkaseComponentName = showkaseAnnotation.name,
         showkaseComponentGroup = showkaseAnnotation.group,
-        showkaseComponentWidthDp = showkaseAnnotation.widthDp,
-        showkaseComponentHeightDp = showkaseAnnotation.heightDp
+        showkaseComponentWidthDp = showkaseAnnotation.widthDp.parseAnnotationProperty(),
+        showkaseComponentHeightDp = showkaseAnnotation.heightDp.parseAnnotationProperty()
     )
+}
+
+internal fun getShowkaseMetadataFromPreview(
+    element: ExecutableElement,
+    elementUtil: Elements,
+    typeUtils: Types,
+    previewTypeMirror: TypeMirror
+): ShowkaseMetadata {
+    val enclosingElement = element.enclosingElement
+    val isStaticMethod = element.modifiers.contains(Modifier.STATIC)
+    val previewAnnotationMirror = element.annotationMirrors.find {
+        typeUtils.isSameType(it.annotationType, previewTypeMirror)
+    }
+
+    val map = mutableMapOf<ShowkaseAnnotationProperty, Any>()
+    previewAnnotationMirror?.elementValues?.map { entry ->
+        val key = entry.key.simpleName.toString().toUpperCase()
+        val value = entry.value.value
+        // Only store the properties that we currently support in the annotation
+        if (ShowkaseAnnotationProperty.values().any { it.name == key }) {
+            // All the supported types are safe to serialize as a String in order to store in the
+            // map.
+            val annotationProperty = 
+                ShowkaseAnnotationProperty.valueOf(key)
+            map[annotationProperty] =  value
+        }
+    }
+    val packageElement = elementUtil.getPackageOf(element)
+    val moduleName = packageElement.simpleName.toString()
+    val packageName = packageElement.qualifiedName.toString()
+    val methodName = element.simpleName.toString()
+
+    val numParameters = element.parameters.size
+    if (numParameters > 0) {
+        throw ShowkaseProcessorException(
+            "Showkase currently only supports functions that do not take in any parameters."
+        )
+    }
+    return ShowkaseMetadata(
+        moduleName = moduleName,
+        packageName = packageName,
+        // If isStaticMethod is true, it means the method was declared at the top level. 
+        // If not, it was declared inside a class
+        // TODO(vinaygaba): Add support for methods inside companion objects and 
+        // objects
+        enclosingClass = if (isStaticMethod) null else enclosingElement.asType(),
+        methodName = methodName,
+        showkaseComponentName = map[ShowkaseAnnotationProperty.NAME]?.let { it as String }.orEmpty(),
+        showkaseComponentGroup = map[ShowkaseAnnotationProperty.GROUP]?.let { it as String }.orEmpty(),
+        showkaseComponentWidthDp = map[ShowkaseAnnotationProperty.WIDTHDP]?.let { it as Int },
+        showkaseComponentHeightDp = map[ShowkaseAnnotationProperty.HEIGHTDP]?.let { it as Int }
+    )
+}
+
+internal enum class ShowkaseAnnotationProperty {
+    NAME,
+    GROUP,
+    WIDTHDP,
+    HEIGHTDP,
 }
