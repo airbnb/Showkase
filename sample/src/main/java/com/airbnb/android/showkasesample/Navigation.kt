cef6a8211880f70b524ff7a9c5cdcd01b885f90d
Vinay Gaba <vinaygaba@gmail.com>
2020-11-02 15:07:33 -0800

    Upgraded compose to use alpha06 (#118)

--- sample/src/main/java/com/airbnb/android/showkasesample/Navigation.kt
+++ sample/src/main/java/com/airbnb/android/showkasesample/Navigation.kt
@@ -1,6 +1,6 @@
 package com.airbnb.android.showkasesample
 
-import androidx.compose.foundation.Icon
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Text
