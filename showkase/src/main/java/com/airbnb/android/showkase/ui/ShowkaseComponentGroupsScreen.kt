7113d2980bce692f8da728faa5da1eb7d858a131
vinaygaba <vinaygaba@gmail.com>
2020-12-19 16:10:50 -0800

    Upgraded to latest

--- showkase/src/main/java/com/airbnb/android/showkase/ui/ShowkaseComponentGroupsScreen.kt
+++ showkase/src/main/java/com/airbnb/android/showkase/ui/ShowkaseComponentGroupsScreen.kt
@@ -1,9 +1,8 @@
 package com.airbnb.android.showkase.ui
 
-import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.navigation.compose.navigate
 import androidx.navigation.NavHostController
 import com.airbnb.android.showkase.models.ShowkaseBrowserComponent
 import com.airbnb.android.showkase.models.ShowkaseBrowserScreenMetadata
@@ -21,21 +20,26 @@ internal fun ShowkaseComponentGroupsScreen(
         showkaseBrowserScreenMetadata
     )
 
-    LazyColumnFor(items = filteredList, itemContent = { group ->
-        SimpleTextCard(
-            text = group, 
-            onClick = {
-                showkaseBrowserScreenMetadata.update {
-                    copy(
-                        currentGroup = group,
-                        isSearchActive = false,
-                        searchQuery = null
-                    )
-                }
-                navController.navigate(ShowkaseCurrentScreen.COMPONENTS_IN_A_GROUP)
+    LazyColumn {
+        items(
+            items = filteredList,
+            itemContent = { group ->
+                SimpleTextCard(
+                    text = group,
+                    onClick = {
+                        showkaseBrowserScreenMetadata.update {
+                            copy(
+                                currentGroup = group,
+                                isSearchActive = false,
+                                searchQuery = null
+                            )
+                        }
+                        navController.navigate(ShowkaseCurrentScreen.COMPONENTS_IN_A_GROUP)
+                    }
+                )
             }
         )
-    })
+    }
     BackButtonHandler {
         goBackToCategoriesScreen(showkaseBrowserScreenMetadata, navController)
     }
--- showkase/src/main/java/com/airbnb/android/showkase/ui/ShowkaseTypographyGroupsScreen.kt
+++ showkase/src/main/java/com/airbnb/android/showkase/ui/ShowkaseTypographyGroupsScreen.kt
@@ -1,9 +1,8 @@
 package com.airbnb.android.showkase.ui
 
-import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.navigation.compose.navigate
 import androidx.navigation.NavHostController
 import com.airbnb.android.showkase.models.ShowkaseBrowserScreenMetadata
 import com.airbnb.android.showkase.models.ShowkaseBrowserTypography
@@ -20,22 +19,27 @@ internal fun ShowkaseTypographyGroupsScreen(
         groupedTypographyMap.keys.sorted(),
         showkaseBrowserScreenMetadata
     )
-    
-    LazyColumnFor(items = filteredList, itemContent = { group ->
-        SimpleTextCard(
-            text = group,
-            onClick = {
-                showkaseBrowserScreenMetadata.update {
-                    copy(
-                        currentGroup = group,
-                        isSearchActive = false,
-                        searchQuery = null
-                    )
-                }
-                navController.navigate(ShowkaseCurrentScreen.TYPOGRAPHY_IN_A_GROUP)
+
+    LazyColumn {
+        items(
+            items = filteredList,
+            itemContent = { group ->
+                SimpleTextCard(
+                    text = group,
+                    onClick = {
+                        showkaseBrowserScreenMetadata.update {
+                            copy(
+                                currentGroup = group,
+                                isSearchActive = false,
+                                searchQuery = null
+                            )
+                        }
+                        navController.navigate(ShowkaseCurrentScreen.TYPOGRAPHY_IN_A_GROUP)
+                    }
+                )
             }
         )
-    })
+    }
     BackButtonHandler {
         goBackToCategoriesScreen(showkaseBrowserScreenMetadata, navController)
     }
