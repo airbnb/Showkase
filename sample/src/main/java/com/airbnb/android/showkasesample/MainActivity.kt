aea99d9cb9d63f1cba6a2a2d76cc13ee5e361f3a
vinaygaba <vinaygaba@gmail.com>
2021-03-17 10:33:57 -1000

    Cleanup

--- sample/src/main/java/com/airbnb/android/showkasesample/MainActivity.kt
+++ sample/src/main/java/com/airbnb/android/showkasesample/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.airbnb.android.showkasesample
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.airbnb.android.showkase.annotation.ShowkaseRoot
-import com.airbnb.android.showkase.models.ShowkaseBrowserComponent
-import com.airbnb.android.showkase.models.ShowkaseProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
