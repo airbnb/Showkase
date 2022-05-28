fe43ca952a13067753e3b7ff42b47a48a5553d6c
Vinay Gaba <vinaygaba@gmail.com>
2022-05-27 17:44:23 -0700

    Add screenshot testing integration with Shot to make introducing screenshot testing in a codebase trivial (#232)
    * Saving changes
    
    * Added screenshots for all api versions
    
    * Update screenshots
    
    * Change mechanism to show errors
    
    * fix package error and add upload artifact job
    
    * Comment for testing
    
    * Test with record mode
    
    * Add job to record and upload screenshots
    
    * Add api level in name of artifact
    
    * Upload screenshots from CI
    
    * Re-enable other jobs
    
    * Clean up and test something
    
    * cleanup
    
    * Re-enable vannitech plugin
    
    * Add vannitech pluging dependencies
    
    * Add vannitech using apply syntax
    
    * nits

---
+++ showkase-screenshot-testing-shot/src/main/java/com.airbnb.android.showkase.screenshot.testing.shot/ShotShowkaseScreenshotTest.kt
@@ -0,0 +1,20 @@
+package com.airbnb.android.showkase.screenshot.testing.shot
+
+import android.graphics.Bitmap
+import com.airbnb.android.showkase.screenshot.testing.ShowkaseScreenshotTest
+import com.airbnb.android.showkase.screenshot.testing.ShowkaseScreenshotType
+import com.karumi.shot.ScreenshotTest
+
+abstract class ShotShowkaseScreenshotTest: ShowkaseScreenshotTest, ScreenshotTest {
+
+    override fun onScreenshot(
+        id: String,
+        name: String,
+        group: String,
+        styleName: String?,
+        screenshotType: ShowkaseScreenshotType,
+        screenshotBitmap: Bitmap
+    ) {
+        compareScreenshot(composeTestRule)
+    }
+}
