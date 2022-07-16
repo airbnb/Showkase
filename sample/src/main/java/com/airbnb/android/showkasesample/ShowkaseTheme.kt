c36802182d4c98d725c10822bd6850f2e84eb3db
Odin Asbjørnsen <odinas004@gmail.com>
2022-07-16 14:43:44 +0200

    Optimize code formatting in ShowkaseTheme.kt

--- sample/src/main/java/com/airbnb/android/showkasesample/ShowkaseTheme.kt
+++ sample/src/main/java/com/airbnb/android/showkasesample/ShowkaseTheme.kt
@@ -5,10 +5,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.colorResource
 
 @Composable
-fun ShowkaseTheme(children: @Composable()() -> Unit) {
+fun ShowkaseTheme(children: @Composable () -> Unit) {
     val light = lightColors()
     val dark = darkColors()
     val colors = if (isSystemInDarkTheme()) { dark } else { light }
