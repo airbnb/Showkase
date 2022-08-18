800939ba30ae1bca9458860016f36c5c10ba6852
Vinay Gaba <vinaygaba@gmail.com>
2022-08-17 21:38:42 -0700

    Run tests even on failure

--- showkase-annotation/src/main/java/com/airbnb/android/showkase/annotation/ShowkaseComposable.kt
+++ showkase-annotation/src/main/java/com/airbnb/android/showkase/annotation/ShowkaseComposable.kt
@@ -56,7 +56,7 @@ package com.airbnb.android.showkase.annotation
  */
 @MustBeDocumented
 @Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.ANNOTATION_CLASS)
 @Suppress("LongParameterList")
 annotation class ShowkaseComposable(
     val name: String = "",
