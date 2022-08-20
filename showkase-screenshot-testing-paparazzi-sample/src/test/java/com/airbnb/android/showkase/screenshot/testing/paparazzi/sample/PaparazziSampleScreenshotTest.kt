ff7da70b8e90043bf0f31e46d833023606cb5445
Vinay Gaba <vinaygaba@gmail.com>
2022-08-19 21:55:46 -0700

    Added colors and typography to the tests as well

--- showkase-screenshot-testing-paparazzi-sample/src/test/java/com/airbnb/android/showkase/screenshot/testing/paparazzi/sample/PaparazziSampleScreenshotTest.kt
+++ showkase-screenshot-testing-paparazzi-sample/src/test/java/com/airbnb/android/showkase/screenshot/testing/paparazzi/sample/PaparazziSampleScreenshotTest.kt
@@ -1,12 +1,18 @@
 package com.airbnb.android.showkase.screenshot.testing.paparazzi.sample
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
 import com.airbnb.android.showkase.models.ShowkaseBrowserComponent
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
@@ -16,17 +22,28 @@ import org.junit.runner.RunWith
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
 import com.airbnb.android.showkase.models.Showkase
+import com.airbnb.android.showkase.models.ShowkaseBrowserColor
+import com.airbnb.android.showkase.models.ShowkaseBrowserTypography
+import com.airbnb.android.showkase.ui.padding4x
+import java.util.*
 
 /**
- * Full credit to Alex Vanyo for creating this sample in the Now In Android app by Google.
- * PR here - https://github.com/android/nowinandroid/pull/101
+ * Credit to Alex Vanyo for creating this sample in the Now In Android app by Google.
+ * PR here - https://github.com/android/nowinandroid/pull/101. Modified the test from that PR to
+ * my own needs for this sample.
  */
 @RunWith(TestParameterInjector::class)
 class PaparazziSampleScreenshotTest {
 
     object PreviewProvider : TestParameter.TestParameterValuesProvider {
-        override fun provideValues(): List<ComponentPreview> =
-            Showkase.getMetadata().componentList.map(::ComponentPreview)
+        override fun provideValues(): List<TestPreview> {
+            val metadata = Showkase.getMetadata()
+            val components = metadata.componentList.map(::ComponentTestPreview)
+            val colors = metadata.colorList.map(::ColorTestPreview)
+            val typography = metadata.typographyList.map(::TypographyTestPreview)
+
+            return components + colors + typography
+        }
     }
 
     enum class BaseDeviceConfig(
@@ -43,7 +60,7 @@ class PaparazziSampleScreenshotTest {
 
     @Test
     fun preview_tests(
-        @TestParameter(valuesProvider = PreviewProvider::class) componentPreview: ComponentPreview,
+        @TestParameter(valuesProvider = PreviewProvider::class) componentTestPreview: TestPreview,
         @TestParameter baseDeviceConfig: BaseDeviceConfig,
         @TestParameter(value = ["1.0", "1.5"]) fontScale: Float
     ) {
@@ -61,16 +78,57 @@ class PaparazziSampleScreenshotTest {
                 )
             ) {
                 Box {
-                    componentPreview.content()
+                    componentTestPreview.Content()
                 }
             }
         }
     }
 }
 
-class ComponentPreview(
+interface TestPreview {
+    @Composable
+    fun Content()
+}
+
+class ComponentTestPreview(
     private val showkaseBrowserComponent: ShowkaseBrowserComponent
-) {
-    val content: @Composable () -> Unit = showkaseBrowserComponent.component
+) : TestPreview {
+    @Composable
+    override fun Content() = showkaseBrowserComponent.component()
     override fun toString(): String = showkaseBrowserComponent.componentKey
+}
+
+class ColorTestPreview(
+    private val showkaseBrowserColor: ShowkaseBrowserColor
+) : TestPreview {
+    @Composable
+    override fun Content() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+                .background(showkaseBrowserColor.color)
+        )
+    }
+
+    override fun toString(): String = "${showkaseBrowserColor.colorGroup}_${showkaseBrowserColor.colorName}"
+}
+
+class TypographyTestPreview(
+    private val showkaseBrowserTypography: ShowkaseBrowserTypography
+) : TestPreview {
+    @Composable
+    override fun Content() {
+        BasicText(
+            text = showkaseBrowserTypography.typographyName.replaceFirstChar {
+                it.titlecase(Locale.getDefault())
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding4x),
+            style = showkaseBrowserTypography.textStyle
+        )
+    }
+
+    override fun toString(): String = "${showkaseBrowserTypography.typographyGroup}_${showkaseBrowserTypography.typographyName}"
 }
