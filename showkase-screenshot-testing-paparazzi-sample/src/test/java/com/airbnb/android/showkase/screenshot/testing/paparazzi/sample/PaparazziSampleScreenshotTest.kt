bc68a9713882b09a36fbe6b2d0d5ccde3e00a8bf
Vinay Gaba <vinaygaba@gmail.com>
2022-09-24 22:27:25 -0700

    Added LocalOnBackPressedDispatcherOwner to avoid crash

--- showkase-screenshot-testing-paparazzi-sample/src/test/java/com/airbnb/android/showkase/screenshot/testing/paparazzi/sample/PaparazziSampleScreenshotTest.kt
+++ showkase-screenshot-testing-paparazzi-sample/src/test/java/com/airbnb/android/showkase/screenshot/testing/paparazzi/sample/PaparazziSampleScreenshotTest.kt
@@ -1,5 +1,8 @@
 package com.airbnb.android.showkase.screenshot.testing.paparazzi.sample
 
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.OnBackPressedDispatcherOwner
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,8 +14,10 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
 import com.airbnb.android.showkase.models.ShowkaseBrowserComponent
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
@@ -70,12 +75,19 @@ class PaparazziSampleScreenshotTest {
             )
         )
         paparazzi.snapshot {
+            val lifecycleOwner = LocalLifecycleOwner.current
             CompositionLocalProvider(
                 LocalInspectionMode provides true,
                 LocalDensity provides Density(
                     density = LocalDensity.current.density,
                     fontScale = fontScale
-                )
+                ),
+                // Needed so that UI that uses it don't crash during screenshot tests
+                LocalOnBackPressedDispatcherOwner provides object: OnBackPressedDispatcherOwner {
+                    override fun getLifecycle() = lifecycleOwner.lifecycle
+
+                    override fun getOnBackPressedDispatcher() = OnBackPressedDispatcher()
+                }
             ) {
                 Box {
                     componentTestPreview.Content()
