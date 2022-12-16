8c3bf634e43b18ca61f43ca0d8d17150933409ad
Stefan Kramreither <me@stefankram.com>
2022-12-16 11:58:19 -0500

    Fix bug in getShowkaseMetadataFromPreview where the preview width was being set as the showkase metadata height (#280)
    * ShowkaseMetadata: Fix bug in getShowkaseMetadataFromPreview where the preview width was being set as the showkase metadata height
    
    * ShowkaseProcessorTest: Add tests to ensure width and height are correctly set in the generated showkase code
    
    * Fix breaking tests
    
    Co-authored-by: Vinay Gaba <vinaygaba@gmail.com>

---
+++ showkase-processor-testing/src/test/resources/ShowkaseProcessorTest/top_level_composable_function_with_showkase_and_showkaseroot_with_width_and_height/output/TestShowkaseRootCodegen.kt
@@ -0,0 +1,42 @@
+// This is an auto-generated file. Please do not edit/modify this file.
+package com.airbnb.android.showkase_processor_testing
+
+import androidx.compose.runtime.Composable
+import com.airbnb.android.showkase.`annotation`.ShowkaseRootCodegen
+import com.airbnb.android.showkase.models.ShowkaseBrowserColor
+import com.airbnb.android.showkase.models.ShowkaseBrowserComponent
+import com.airbnb.android.showkase.models.ShowkaseBrowserTypography
+import com.airbnb.android.showkase.models.ShowkaseProvider
+import kotlin.collections.List
+
+@ShowkaseRootCodegen(
+  numComposablesWithoutPreviewParameter = 1,
+  numComposablesWithPreviewParameter = 0,
+  numColors = 0,
+  numTypography = 0,
+)
+public class TestShowkaseRootCodegen : ShowkaseProvider {
+  public val componentList: List<ShowkaseBrowserComponent> =
+      mutableListOf<ShowkaseBrowserComponent>(
+        ShowkaseBrowserComponent(
+            group = "group",
+            componentName = "name",
+            componentKDoc = "",
+            componentKey =
+                """com.airbnb.android.showkase_processor_testing_null_group_name_0_null""",
+            isDefaultStyle = false,
+            widthDp = 200,
+            heightDp = 300,
+            component = @Composable { TestComposable() })
+      )
+
+  public val colorList: List<ShowkaseBrowserColor> = listOf<ShowkaseBrowserColor>()
+
+  public val typographyList: List<ShowkaseBrowserTypography> = listOf<ShowkaseBrowserTypography>()
+
+  public override fun getShowkaseComponents(): List<ShowkaseBrowserComponent> = componentList
+
+  public override fun getShowkaseColors(): List<ShowkaseBrowserColor> = colorList
+
+  public override fun getShowkaseTypography(): List<ShowkaseBrowserTypography> = typographyList
+}
