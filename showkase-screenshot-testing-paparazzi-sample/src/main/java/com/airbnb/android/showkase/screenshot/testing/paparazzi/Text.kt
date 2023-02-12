f9ea450a751001ae6fc4ddc2298f4039db641b28
Odin Asbjørnsen <odinas004@gmail.com>
2023-02-12 13:55:29 +0100

    Add paparazzi test for custom preview annotation

---
+++ showkase-screenshot-testing-paparazzi-sample/src/main/java/com/airbnb/android/showkase/screenshot/testing/paparazzi/Text.kt
@@ -0,0 +1,20 @@
+package com.airbnb.android.showkase.screenshot.testing.paparazzi
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+// This is for testing that custom annotation
+// works with paparazzi
+@HighFont
+@Composable
+fun Preview() {
+    Text(
+        modifier = Modifier.background(color = Color.White).fillMaxWidth(),
+        text = "Gustav"
+    )
+}
