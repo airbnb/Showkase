c3b7f7db113b6f519d1ce7d7d11a9519638cb7ba
Odin Asbjørnsen <odinas004@gmail.com>
2023-03-04 18:54:26 +0100

    Add missing compiler arg in test

--- showkase-browser-testing-submodule-2/src/main/java/com/vinaygaba/showkase_browser_testing_submodule/two/LocalePreview.kt
+++ showkase-browser-testing-submodule-2/src/main/java/com/vinaygaba/showkase_browser_testing_submodule/two/LocalePreview.kt
@@ -4,7 +4,7 @@ package com.vinaygaba.showkase_browser_testing_submodule.two
 
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(name = "English", group = "En LocalePreview", locale = "en")
+@Preview(name = "English", group = "LocalePreview", locale = "en")
 annotation class EnglishLocalePreview
 
 @Preview(name = "Norwegian", group = "LocalePreview", locale = "no")
