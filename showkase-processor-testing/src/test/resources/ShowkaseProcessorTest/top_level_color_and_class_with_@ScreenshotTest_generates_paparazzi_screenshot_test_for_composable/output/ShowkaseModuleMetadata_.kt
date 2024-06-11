75edf88d51f8dedfc98d3760cef672521af395cf
Vinay Gaba <vinaygaba@gmail.com>
2024-06-11 00:24:27 -0700

    Update tests

---
+++ showkase-processor-testing/src/test/resources/ShowkaseProcessorTest/top_level_color_and_class_with_@ScreenshotTest_generates_paparazzi_screenshot_test_for_composable/output/ShowkaseModuleMetadata_.kt
@@ -0,0 +1,27 @@
+// This is an auto-generated file. Please do not edit/modify this file.This is an auto-generated file. Please do not edit/modify this file.
+import com.airbnb.android.showkase.models.ShowkaseBrowserColor
+import com.airbnb.android.showkase.models.ShowkaseBrowserComponent
+import com.airbnb.android.showkase.models.ShowkaseBrowserTypography
+import com.airbnb.android.showkase.models.ShowkaseProvider
+import kotlin.collections.List
+
+public class ShowkaseModuleMetadata_ : ShowkaseProvider {
+  override fun getShowkaseComponents(): List<ShowkaseBrowserComponent> {
+
+    return listOf<ShowkaseBrowserComponent>(
+    )
+  }
+
+  override fun getShowkaseColors(): List<ShowkaseBrowserColor> {
+
+    return listOf<ShowkaseBrowserColor>(
+        redgroupname,
+    )
+  }
+
+  override fun getShowkaseTypography(): List<ShowkaseBrowserTypography> {
+
+    return listOf<ShowkaseBrowserTypography>(
+    )
+  }
+}
