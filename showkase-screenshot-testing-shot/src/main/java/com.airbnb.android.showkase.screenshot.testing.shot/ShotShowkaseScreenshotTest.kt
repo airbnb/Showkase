c710d64df9cb4be8cc54f1450455b3a0c8a9360e
Vinay Gaba <vinaygaba@gmail.com>
2024-12-11 16:43:33 +0530

    Fix lint error

--- showkase-screenshot-testing-shot/src/main/java/com.airbnb.android.showkase.screenshot.testing.shot/ShotShowkaseScreenshotTest.kt
+++ showkase-screenshot-testing-shot/src/main/java/com.airbnb.android.showkase.screenshot.testing.shot/ShotShowkaseScreenshotTest.kt
@@ -1,12 +1,15 @@
 package com.airbnb.android.showkase.screenshot.testing.shot
 
 import android.graphics.Bitmap
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.airbnb.android.showkase.screenshot.testing.ShowkaseScreenshotTest
 import com.airbnb.android.showkase.screenshot.testing.ShowkaseScreenshotType
 import com.karumi.shot.ScreenshotTest
 
 abstract class ShotShowkaseScreenshotTest : ShowkaseScreenshotTest, ScreenshotTest {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onScreenshot(
         id: String,
         name: String,
