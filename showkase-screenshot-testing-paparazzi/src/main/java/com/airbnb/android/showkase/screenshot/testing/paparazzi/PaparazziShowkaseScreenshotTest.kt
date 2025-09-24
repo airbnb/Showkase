6616825ef9fa7b9bfe9a6fb282f80095f53a40e4
Allen Chen <allenchen1154@gmail.com>
2025-09-24 15:15:32 -0500

    Add a `Skip` option to screenshot configuration
    This can be useful for excluding certain annotated previews from being screenshot tested, for example when `layoutlib` cannot correctly render the contents, but when it's still useful to see the component in the Showkase browser.

--- showkase-screenshot-testing-paparazzi/src/main/java/com/airbnb/android/showkase/screenshot/testing/paparazzi/PaparazziShowkaseScreenshotTest.kt
+++ showkase-screenshot-testing-paparazzi/src/main/java/com/airbnb/android/showkase/screenshot/testing/paparazzi/PaparazziShowkaseScreenshotTest.kt
@@ -124,6 +124,7 @@ interface PaparazziShowkaseScreenshotTest {
                  end = captureType.durationMillis.toLong(),
                  fps = captureType.framerate
              )
+             ScreenshotConfig.Skip -> return
          }
     }
 
